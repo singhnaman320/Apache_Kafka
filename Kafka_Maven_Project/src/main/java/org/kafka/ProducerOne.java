ad3a39037f83c6738524d0ea5b3bfc1e7b15e2ad
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-04 17:23:39 +0530

    Modifying pom.xml and producer

--- Kafka_Maven_Project/src/main/java/org/kafka/ProducerOne.java
+++ Kafka_Maven_Project/src/main/java/org/kafka/ProducerOne.java
@@ -1,8 +1,21 @@
 package org.kafka;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+/*
+   
+*/
 public class ProducerOne {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        String bootstrapServers = "127.0.0.1:9092";
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
     }
 }
