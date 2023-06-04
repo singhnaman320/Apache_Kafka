14d6cfa85b7e7ce2c08d053b7b7d1e6e19f26ee4
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-04 17:25:02 +0530

    Creating properties

--- Kafka_Maven_Project/src/main/java/org/kafka/ProducerOne.java
+++ Kafka_Maven_Project/src/main/java/org/kafka/ProducerOne.java
@@ -6,13 +6,17 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import java.util.Properties;
 
 /*
+    1. bootstrap.servers: It is a list of the port pairs which are used for establishing an initial connection to the
+       Kafka cluster. The users can use the bootstrap servers only for making an initial connection only. This server
+       is present in the host:port, host:port,... form.
 
+   
 */
 public class ProducerOne {
     public static void main(String[] args) {
 
         // Creating properties
-        
+
         String bootstrapServers = "127.0.0.1:9092";
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
