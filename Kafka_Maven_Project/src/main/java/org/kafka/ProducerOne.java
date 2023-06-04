908393b2f774bc43771d0c6bd3985d3b60e6c977
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-04 18:12:32 +0530

    Creating producer

--- Kafka_Maven_Project/src/main/java/org/kafka/ProducerOne.java
+++ Kafka_Maven_Project/src/main/java/org/kafka/ProducerOne.java
@@ -1,5 +1,6 @@
 package org.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
@@ -19,7 +20,7 @@ import java.util.Properties;
 public class ProducerOne {
     public static void main(String[] args) {
 
-        // Creating properties
+        // =-=-= Creating properties =-=-=
         // When we create the properties, it imports the 'java.util.Properties' to the code.
 
         String bootstrapServers = "127.0.0.1:9092";
@@ -28,7 +29,23 @@ public class ProducerOne {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        // Creating Producer
+        // =-=-= Creating Producer =-=-=
+
+        // To create a Kafka producer, we just need to create an object of KafkaProducer.
+        // The object of KafkaProducer can be created as:
+
+        KafkaProducer<String, String> firstProducer = new KafkaProducer<>(properties);
+
+        // Here, 'firstProducer' is the name of the producer.
+
+        // =-=-= Creating the Producer Record =-=-=
+
+        /* In order to send the data to Kafka, the user need to create a ProducerRecord. It is because all the producers
+           lie inside a producer record. Here, the producer specifies the topic name as well as the message which is to
+           be delivered to Kafka.
+         */
         
+        // A ProducerRecord can be created as:
+
     }
 }
