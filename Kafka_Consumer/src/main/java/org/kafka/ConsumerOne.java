edbf22dc122c6d06b2ccedcd1fc81e2a0c1b0ae4
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-10 15:11:26 +0530

    Consumer Created

---
+++ Kafka_Consumer/src/main/java/org/kafka/ConsumerOne.java
@@ -0,0 +1,21 @@
+package org.kafka;
+
+/*
+
+[] Creating Kafka Consumer in Java:
+
+    There are following steps taken to create a consumer:
+
+    1. Create Logger
+    2. Create consumer properties.
+    3. Create a consumer.
+    4. Subscribe the consumer to a specific topic.
+    5. Poll for some new data
+
+*/
+public class ConsumerOne {
+    public static void main(String[] args) {
+
+
+    }
+}
