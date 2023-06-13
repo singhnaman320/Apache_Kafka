b52f6afb6d13a69da07926e35d66e93312e3f177
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-13 13:16:35 +0530

    Producing consumer

--- Kafka_Consumer/src/main/java/org/kafka/ConsumerOne.java
+++ Kafka_Consumer/src/main/java/org/kafka/ConsumerOne.java
@@ -1,10 +1,13 @@
 package org.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ConsumerOne {
     public static void main(String[] args) {
 
         /* []=-=-= Creating Logger =-=-=[] */
 
-        
+        Logger logger = LoggerFactory.getLogger(ConsumerOne.class);
     }
 }
