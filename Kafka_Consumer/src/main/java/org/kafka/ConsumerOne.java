7bc4682e76b2f69f1c0ea6c34eb3ebf9b4bfca77
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-13 14:03:45 +0530

    Producing consumer

--- Kafka_Consumer/src/main/java/org/kafka/ConsumerOne.java
+++ Kafka_Consumer/src/main/java/org/kafka/ConsumerOne.java
@@ -21,7 +21,11 @@ public class ConsumerOne {
         // Creating consumer properties
 
         Properties properties = new Properties();
+
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
     }
 }
