b59f558f18f06a081faefde63025f9ce1ba3f6a5
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-13 13:48:31 +0530

    Producing consumer

--- Kafka_Consumer/src/main/java/org/kafka/ConsumerOne.java
+++ Kafka_Consumer/src/main/java/org/kafka/ConsumerOne.java
@@ -1,7 +1,11 @@
 package org.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
 
 public class ConsumerOne {
     public static void main(String[] args) {
@@ -13,5 +17,11 @@ public class ConsumerOne {
         String bootstrapServers = "localhost:9092";
         String groupId = "secApp";
         String topic = "TopicOne";
+
+        // Creating consumer properties
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     }
 }
